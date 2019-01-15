8838db2b8198253c820f1f8332a2a5d78d60dcd1
„Oliver <b.oliver94@googlemail.com>
2019-01-15 12:09:56 +0100

    Created frequency-proteins split violinplot

--- ppi-plots.py
+++ ppi-plots.py
@@ -10,8 +10,6 @@ import seaborn as sns
 sns.set(color_codes=True)
 import Bio  # https://biopython.org/wiki/SeqIO
 
-# %%
-
 
 # get the right path
 import inspect
@@ -33,9 +31,6 @@ for record in SeqIO.parse(seq_path, "fasta"):
 prot_series = pd.Series(prot_list)
 
 
-# %%
-
-# Creating a length series for the amino acids
 seq_len = []
 for element in prot_list:
     seq_len.append(len(element))
@@ -55,85 +50,145 @@ data = {"Labels" : label_list, "Length" : seq_len, "Sequence" : prot_list}
 prot_df = pd.DataFrame(data, columns = ["Labels", "Length", "Sequence"])
 
 # changing the bind/nonbind labels to numerical values
-prot_df["Labels"] = prot_df["Labels"].apply(lambda x: 1 if x == "Bind" else 0)
+# prot_df["Labels"] = prot_df["Labels"].apply(lambda x: 1 if x == "Bind" else 0)
 
-print("here1")
 print(prot_df.count())
 
-# Sorting out all the duplicate entries # TODO: fix that to much of the rows are dropped, result should be 905
-# prot_df = prot_df.drop_duplicates().reset_index()
+# Sorting out all the duplicate entries
+prot_df = prot_df.drop_duplicates().reset_index()
+prot_df = prot_df.replace("X", "G")
 
 
-print("here2")
 print(prot_df.count())
 
 
 # print(label_series.describe()) # Non_Bind: 651; Bind: 536
 
 
-#%%
-# Plotting the average distance between amino acids
+
+# %%
+#print(os.chdir("/Users/universal/Google\ Drive/GOOGLE\ DRIVE/UNIVERSITY/BIOINFORMATICS/BIOINFO\ -\ PBL/ML-Protein-Protein-Interaction/plots"))
 
 
-prot_labels = {"C_avg_d", "D_avg_d", "S_avg_d", "Q_avg_d", "K_avg_d", "I_avg_d", "P_avg_d", "T_avg_d", "F_avg_d", "N_avg_d", "G_avg_d", "H_avg_d", "L_avg_d", "R_avg_d", "W_avg_d", "A_avg_d", "V_avg_d", "E_avg_d", "Y_avg_d", "M_avg_d","X_avg_d", "Z_avg_d"}
-prot_list = {"C", "D", "S", "Q", "K", "I", "P", "T", "F", "N", "G", "H", "L", "R", "W", "A", "V", "E", "Y", "M","X", "Z"}
+# Hypothesis 1: protein length is in relation to binding properties
+plt.figure
+ax = sns.violinplot(x="Labels", y="Length", data=prot_df)
+plt.savefig("violinplot_len-and-labels", dpi=300)
+plt.show()
 
-avg_dist_df = pd.DataFrame(columns=prot_labels)
-avg_dist_df
+# %% calculate avg length of binding and non binding prot
+# prot_df
 
-part_to_add = "_avg_d"
+# interacting
+bind = prot_df.loc[prot_df["Labels"] == "Bind"]
+print(bind["Length"].mean())
 
-first = 0
+# non interacting
+non_bind = prot_df.loc[prot_df["Labels"] == "Non_Bind"]
+print(non_bind["Length"].mean())
+
+# %%
+# Hypothesis 2: frequency of amino acids influences binding properties
+
+dict_list = []
+
 
 for i in range(len(prot_df)):
-    seq = prot_df.iat[i, 2]
-    prot_dict = {"C_avg_d": 0, "D_avg_d": 0, "S_avg_d": 0, "Q_avg_d": 0, "K_avg_d": 0, "I_avg_d": 0, "P_avg_d": 0,
-                 "T_avg_d": 0, "F_avg_d": 0, "N_avg_d": 0, "G_avg_d": 0, "H_avg_d": 0, "L_avg_d": 0, "R_avg_d": 0,
-                 "W_avg_d": 0, "A_avg_d": 0, "V_avg_d": 0, "E_avg_d": 0, "Y_avg_d": 0, "M_avg_d": 0, "X_avg_d": 0,
-                 "Z_avg_d": 0}
-    for prot in prot_list:
-        avg_dist = 0
-        freq_count = 0
-        dist_count = 0
+    seq = prot_df.iat[i,3]
+    prot_dict = {"C": 0, "D": 0, "S": 0, "Q": 0, "K": 0, "I": 0, "P": 0, "T": 0, "F": 0, "N": 0, "G": 0, "H": 0, "L": 0,
+                 "R": 0, "W": 0, "A": 0, "V": 0, "E": 0, "Y": 0, "M": 0, "X": 0, "Z": 0}
+    for char in seq:
+        prot_dict[char] += 1
+    prot_dict["G"] += prot_dict["X"]
+    prot_dict["X"] = 0
+    prot_dict["G"] += prot_dict["Z"]
+    prot_dict["Z"] = 0
+    dict_list.append(prot_dict)
+
+dict_list
 
 
-        for char in seq:
-            if char == prot:
-                freq_count += 1
+# creating a new df for the freq_dicts to then append it to the master df
+freq_df = pd.DataFrame()
 
-            if freq_count >= 1:
-                dist_count += 1
-        if freq_count >= 1:
-            avg_dist = freq_count / dist_count
-            prot_add = prot + part_to_add
-            prot_dict[prot_add] = avg_dist
-            if first == 0:
-                print(prot_dict)
-                first = 1
-        avg_dist_df = avg_dist_df.append(prot_dict, ignore_index=True)
+for dict_dict in dict_list:
+    freq_df = freq_df.append(dict_dict, ignore_index=True)
 
-        # TODO fix the wrong value error, first sequence definitely contains a K
+freq_df = freq_df.drop(["X"], axis=1)
+freq_df = freq_df.drop(["Z"], axis=1)
+
+print(freq_df)
 
 
-print(prot_df.iat[0, 2])
+prot_df = pd.concat([prot_df, freq_df], axis=1, join_axes=[prot_df.index])
+print(prot_df)
+
+
+
+# for dict_dict in dict_list:
+#     for k, v in dict_dict.items():
+#         if (k == "X" and v > 0):
+#             print(v)
 
 
 # %%
 
-# Hypothesis 1: protein length is in relation to binding properties
-ax = sns.boxplot(x="Labels", y="Length", data=prot_df)
+
+# %% Melting the dataframe to be able to plot things
+
+temp_df = prot_df.drop(["index", "Length", "Sequence"], axis=1)
+
+df = pd.melt(temp_df, id_vars="Labels")
+df
+df = df.dropna(axis=0, how="any", thresh=None, subset=None, inplace=False)
+
+print(len(df))
+print(df.iat[18099,0])
+
+df
+
+
+# %%
+
+sns.set(rc={'figure.figsize':(20,8.27)})
+plt.rcParams["axes.labelsize"] = 20
+sns.set(font_scale = 2)
+
+ax = sns.violinplot(width=0.9, gridsize=100, x="variable", y="value", hue="Labels", data=df, palette="muted", split=True)
+ax.set(xlabel='Proteins', ylabel='Frequency')
+plt.setp(ax.get_legend().get_texts(), fontsize='17')
+plt.savefig("frequency_proteins_violin", dpi=300)
 plt.show()
-plt.savefig("boxplot_len-and-labels")
 
-# Hypothesis 2: frequency of amino acids influences binding properties
-frequency_dicts = []
-for element in prot_list:
-    prot_dict = {"C" : 0, "D" : 0, "S" : 0, "Q" : 0, "K" : 0, "I" : 0, "P" : 0, "T" : 0, "F" : 0, "N" : 0, "G" : 0, "H" : 0, "L" : 0, "R" : 0, "W" : 0, "A" : 0, "V" : 0, "E" : 0, "Y" : 0, "M" : 0, "X" : 0, "Z" : 0}
 
-    for char in element:
-        prot_dict[char] += 1
-    frequency_dicts.append(prot_dict)
+# %%
 
+
+x_names = ["C", "D", "S", "Q", "K", "I", "P", "T", "F", "N", "G", "H", "L", "R", "W", "A", "V", "E", "Y", "M"]
+
+#ax = sns.violinplot(x=df.index, y=prot_df[["A", "B"]], hue="Labels", data=prot_df, palette="muted", split=True, size=10)
+
+plt.show()
+print("here")
+
+# %%
+
+df = pd.DataFrame({'X_Axis':[1,3,5,7,10,20],
+                   'col_2':[.4,.5,.4,.5,.5,.4],
+                   'col_3':[.7,.8,.9,.4,.2,.3],
+                   'col_4':[.1,.3,.5,.7,.1,.0],
+                   'col_5':[.5,.3,.6,.9,.2,.4]})
+
+print (df)
+
+df = df.melt('X_Axis', var_name='cols',  value_name='vals')
+
+print(df)
+g = sns.catplot(x="X_Axis", y="vals", hue='cols', data=df)
+
+
+
+# %%
 # frequency_lists = [["C"], ["D"], ["S"], ["Q"], ["K"], ["I"], ["P"], ["T"], ["F"], ["N"], ["G"], ["H"], ["L"], ["R"], ["W"], ["A"], ["V"], ["E"], ["Y"], ["M"], ["X"], ["Z"]]
 frequency_lists = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
 for dict in frequency_dicts:
@@ -143,6 +198,9 @@ for dict in frequency_dicts:
         frequency_lists[counter].append(value)
         counter += 1
 
+print(frequency_lists)
+# %%
+
 
 # Combining frequency lists into one data frame
 data = {"C Frequency" : frequency_lists[0], "D Frequency" : frequency_lists[1], "S Frequency" : frequency_lists[2], "Q Frequency" : frequency_lists[3], "K Frequency" : frequency_lists[4], "I Frequency" : frequency_lists[5], "P Frequency" : frequency_lists[6], "T Frequency" : frequency_lists[7],
@@ -150,6 +208,9 @@ data = {"C Frequency" : frequency_lists[0], "D Frequency" : frequency_lists[1], 
         "A Frequency" : frequency_lists[15], "V Frequency" : frequency_lists[16], "E Frequency" : frequency_lists[17], "Y Frequency" : frequency_lists[18], "M Frequency" : frequency_lists[19], "X Frequency" : frequency_lists[20], "Z Frequency" : frequency_lists[21], "Length" : seq_len, "Labels" : label_list}
 
 freq_df = pd.DataFrame(data, columns = ["C Frequency", "D Frequency", "S Frequency", "Q Frequency", "K Frequency", "I Frequency", "P Frequency", "T Frequency", "F Frequency", "N Frequency", "G Frequency", "H Frequency", "L Frequency", "R Frequency", "W Frequency", "A Frequency", "V Frequency", "E Frequency", "Y Frequency", "M Frequency", "X Frequency", "Z Frequency", "Length", "Labels"])
+
+
+
 
 # Trying the Boxplot with the different frequencies
 # Draw a nested boxplot to show bills by day and time
@@ -168,7 +229,6 @@ plt.savefig('S.png', dpi = 100)
 plt.show()
 
 ax = sns.violinplot(x="Labels", y="Q Frequency", data=freq_df)
-plt.savefig('Q.png', dpi = 100)
 plt.show()
 
 ax = sns.violinplot(x="Labels", y="K Frequency", data=freq_df)
@@ -244,3 +304,52 @@ plt.savefig('Z.png', dpi = 100)
 plt.show()
 
 # TODO: consider group box plot as well
+
+
+#%% # Plotting the average distance between amino acids
+
+for element in prot_series:
+    element
+
+
+prot_labels = {"C_avg_d", "D_avg_d", "S_avg_d", "Q_avg_d", "K_avg_d", "I_avg_d", "P_avg_d", "T_avg_d", "F_avg_d", "N_avg_d", "G_avg_d", "H_avg_d", "L_avg_d", "R_avg_d", "W_avg_d", "A_avg_d", "V_avg_d", "E_avg_d", "Y_avg_d", "M_avg_d","X_avg_d", "Z_avg_d"}
+prot_list = {"C", "D", "S", "Q", "K", "I", "P", "T", "F", "N", "G", "H", "L", "R", "W", "A", "V", "E", "Y", "M","X", "Z"}
+
+avg_dist_df = pd.DataFrame(columns=prot_labels)
+avg_dist_df
+
+part_to_add = "_avg_d"
+
+first = 0
+
+for i in range(len(prot_df)):
+    seq = prot_df.iat[i, 2]
+    prot_dict = {"C_avg_d": 0, "D_avg_d": 0, "S_avg_d": 0, "Q_avg_d": 0, "K_avg_d": 0, "I_avg_d": 0, "P_avg_d": 0,
+                 "T_avg_d": 0, "F_avg_d": 0, "N_avg_d": 0, "G_avg_d": 0, "H_avg_d": 0, "L_avg_d": 0, "R_avg_d": 0,
+                 "W_avg_d": 0, "A_avg_d": 0, "V_avg_d": 0, "E_avg_d": 0, "Y_avg_d": 0, "M_avg_d": 0, "X_avg_d": 0,
+                 "Z_avg_d": 0}
+    for prot in prot_list:
+        avg_dist = 0
+        freq_count = 0
+        dist_count = 0
+
+
+        for char in seq:
+            if char == prot:
+                freq_count += 1
+
+            if freq_count >= 1:
+                dist_count += 1
+        if freq_count >= 1:
+            avg_dist = freq_count / dist_count
+            prot_add = prot + part_to_add
+            prot_dict[prot_add] = avg_dist
+            if first == 0:
+                print(prot_dict)
+                first = 1
+        avg_dist_df = avg_dist_df.append(prot_dict, ignore_index=True)
+
+        # TODO fix the wrong value error, first sequence definitely contains a K
+
+
+print(prot_df.iat[0, 2])
